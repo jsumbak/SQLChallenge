040ae129294732a4c4c763ff22f162d6b88ef870
jsumbak <julsumba311@gmail.com>
2022-07-23 16:52:42 -0700

    pushing current changes

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,11 @@
-INSERT INTO department (names, department_id)
-VALUES ("Sales", "4"), ("Engineering", "8"), ("Finance", "6"), ("Legal", "10");
+/* array for departments */
+INSERT INTO department (names)
+VALUES ("Sales"), ("Engineering"), ("Finance"), ("Legal");
 
+/* array for roles */
 INSERT INTO roles (title, role_id, department_id, salary)
 VALUES ("Lawyer", "1", "10", "200000"), ("Lead Enginner", "2", "8", "150000"), ("Account Manager", "3", "6", "160000"), ("Salesperson", "4", "4", "80000"), ("Sales Lead", "5", "4", "100000");
 
+/* array for employees */
 INSERT INTO employee (first_name, last_name, title, department_id, salary, manager_id)
 VALUES ("Kim", "Kardash", "Lawyer", "Legal", "200000", "Null"), ("Khloe", "Kardash", "Lead Engineer", "Engineering", "150000", "Tristan"), ("Kris", "Jenner", "Account Manager", "Finance", "160000", "Kim"), ("North", "West", "Salesperson", "Sales", "80000", "Kanye"), ("Lord", "Disick", "Sales Lead", "Sales", "100000", "Kourt");
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
-require("console.table");
-const mysql = require("mysql");
+const table = require("console.table");
+const mysql = require('mysql');
 
 // mysql connection
 const connection = mysql.createConnection({
@@ -12,13 +12,149 @@ const connection = mysql.createConnection({
 });
 
 // user is prompted to choose what they would first like to do
-function initialQuestion() {
+function menu() {
     inquirer.prompt({
         type: 'list',
         message: 'What would you like to do?',
         name: 'menu',
-        choices: ["View all departments", "View all roles", "View all employees", "Add an employee", "Update an employee role", "Quit"]
-    })
-   
-};
+        choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role", "Quit"]
+    }).then((answers) => {
+        console.log(answers);
 
+        if (answers.menu === "View all departments") {
+            db.query(`SELECT * FROM deprtment`, (err, result) => {
+                if (err) throw err;
+                console.log("Viewing All Departments: ");
+                console.table(result);
+                employee_db();
+            });
+        } else if (answers.menu === "View all roles") {
+            db.query(`SELECT * FROM roles`, (err, result) => {
+                if (err) throw err;
+                console.log("Viewing All Roles: ");
+                console.table(result);
+                employee_db();
+            });
+        } else if (answers.menu === "View all employees") {
+            db.query(`SELECT * FROM employee`, (err, result) => {
+                if (err) throw err;
+                console.log("Viewing All Employees: ");
+                console.table(result);
+                employee_db();
+            });
+        } else if (answers.menu === "Add a department") { //adding department
+            inquirer.prompt([{
+                type: 'input',
+                name: 'department',
+                message: 'What is the name of the department?',
+                validate: departmentInput => {
+                    if (departmentInput) {
+                        return true;
+                    } else {
+                        console.log('Please add department name.');
+                        return false;
+                    }
+                }
+            }])
+                .then((answers) => {
+                    db.query(`INSERT INTO department (name) VALUES (?)`, [answers.department], (err, result) => {
+                        if (err) throw err;
+                        console.log(`Added ${answers.department} to the database.`)
+                        employee_db();
+                    });
+                })
+        } else if (answers.menu === "Add a role") { //adding role
+            db.query(`SELECT * FROM roles`, (err, result) => {
+                if (err) throw err;
+                inquirer.prompt([{
+                    type: 'input',
+                    name: 'role',
+                    message: 'What is the name of the role?',
+                    validate: rolesInput => {
+                        if (rolesInput) {
+                            return true;
+                        } else {
+                            console.log('Please add role name.');
+                            return false;
+                        }
+                    }
+                }])
+                .then((answers) => {
+                    db.query(`INSERT INTO roles (title) VALUES (?)`, [answers.roles], (err, result) => {
+                        if (err) throw err;
+                        console.log(`Added ${answers.roles} to the database.`)
+                        employee_db();
+                    });
+                })
+            });
+
+            } else if (answers.menu === "Add an employee") {  //adding employee first name
+                db.query(`SELECT * FROM employee`, (err, result) => {
+                    if (err) throw err;
+                    inquirer.prompt([{
+                        type: 'input',
+                        name: 'employee',
+                        message: 'Please insert employee first name.',
+                        validate: employeeInput => {
+                            if (employeeInput) {
+                                return true;
+                            } else {
+                                console.log('Please add employee name.');
+                                return false;
+                            }
+                        }
+                    },
+                    {
+                        // Adding Employee Last Name
+                        type: 'input',
+                        name: 'lastName',
+                        message: 'What is the employees last name?',
+                        validate: lastNameInput => {
+                            if (lastNameInput) {
+                                return true;
+                            } else {
+                                console.log('Please add a last name!');
+                                return false;
+                            }
+                        }
+                    },
+                    {
+                        // Adding Employee Role
+                        type: 'list',
+                        name: 'role',
+                        message: 'What is the employees role?', // WORK ON THIS
+
+                    
+                    },
+                    {
+                        // Adding Employee Manager
+                        type: 'input',
+                        name: 'manager',
+                        message: 'Who is the employees manager?',
+                        validate: managerInput => {
+                            if (managerInput) {
+                                return true;
+                            } else {
+                                console.log('Please add a amanager!');
+                                return false;
+                            }
+                        }
+                    }
+                ]).then((answers) => {
+                        // Comparing the result and storing it into the variable
+                        for (var i = 0; i < result.length; i++) {
+                            if (result[i].title === answers.roles) {
+                                var roles = result[i];
+                            }
+                        }
+                        db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`, [answers.firstName, answers.lastName, role.id, answers.manager.id], (err, result) => {
+                            if (err) throw err;
+                            console.log(`Added ${answers.firstName} ${answers.lastName} to the database.`)
+                            employee_tracker();
+                        });
+                    })
+            });
+
+            // create "update employee"
+            // crete "quit" option
+}
